e3d1661d98508e147963d08f89ae2a9995b4b68c
Abirullah <abirafridi87@gmail.com>
2025-10-29 11:30:30 +0500

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import svgr from '@svgr/rollup';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Portfolio-with-react-main/',
+  base: '/Portfolio-with-react/',
   plugins: [react(), svgr()],
   server: {
     port: 3000,
